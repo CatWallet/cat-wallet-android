db4b9f721c9a70d6b80e36db922d6a71ec6d1041
Yusheng Ding <davidthinkleding@Gmail.com>
2018-08-21 15:31:12 -0700

    feature: add total balance display in send activity

--- app/src/main/java/com/wallet/crypto/trustapp/di/SendModule.java
+++ app/src/main/java/com/wallet/crypto/trustapp/di/SendModule.java
@@ -1,5 +1,10 @@
 package com.wallet.crypto.trustapp.di;
 
+import com.wallet.crypto.trustapp.interact.FindDefaultNetworkInteract;
+import com.wallet.crypto.trustapp.interact.FindDefaultWalletInteract;
+import com.wallet.crypto.trustapp.interact.GetDefaultWalletBalance;
+import com.wallet.crypto.trustapp.repository.EthereumNetworkRepositoryType;
+import com.wallet.crypto.trustapp.repository.WalletRepositoryType;
 import com.wallet.crypto.trustapp.router.ConfirmationRouter;
 import com.wallet.crypto.trustapp.viewmodel.SendViewModelFactory;
 
@@ -9,12 +14,30 @@ import dagger.Provides;
 @Module
 class SendModule {
     @Provides
-    SendViewModelFactory provideSendViewModelFactory(ConfirmationRouter confirmationRouter) {
-        return new SendViewModelFactory(confirmationRouter);
+    SendViewModelFactory provideSendViewModelFactory(ConfirmationRouter confirmationRouter,  GetDefaultWalletBalance getDefaultWalletBalance,
+                                                     FindDefaultNetworkInteract findDefaultNetworkInteract, FindDefaultWalletInteract findDefaultWalletInteract) {
+        return new SendViewModelFactory(confirmationRouter, getDefaultWalletBalance, findDefaultNetworkInteract, findDefaultWalletInteract);
     }
 
     @Provides
     ConfirmationRouter provideConfirmationRouter() {
         return new ConfirmationRouter();
+    }
+
+    @Provides
+    GetDefaultWalletBalance provideGetDefaultWalletBalance(
+            WalletRepositoryType walletRepository, EthereumNetworkRepositoryType ethereumNetworkRepository) {
+        return new GetDefaultWalletBalance(walletRepository, ethereumNetworkRepository);
+    }
+
+    @Provides
+    FindDefaultNetworkInteract provideFindDefaultNetworkInteract(
+            EthereumNetworkRepositoryType ethereumNetworkRepositoryType) {
+        return new FindDefaultNetworkInteract(ethereumNetworkRepositoryType);
+    }
+
+    @Provides
+    FindDefaultWalletInteract provideFindDefaultWalletInteract(WalletRepositoryType walletRepository) {
+        return new FindDefaultWalletInteract(walletRepository);
     }
 }
--- app/src/main/java/com/wallet/crypto/trustapp/viewmodel/SendViewModel.java
+++ app/src/main/java/com/wallet/crypto/trustapp/viewmodel/SendViewModel.java
@@ -1,24 +1,86 @@
 package com.wallet.crypto.trustapp.viewmodel;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
 
+import com.wallet.crypto.trustapp.R;
+import com.wallet.crypto.trustapp.entity.NetworkInfo;
 import com.wallet.crypto.trustapp.entity.Transaction;
 import com.wallet.crypto.trustapp.entity.Wallet;
+import com.wallet.crypto.trustapp.interact.FindDefaultNetworkInteract;
+import com.wallet.crypto.trustapp.interact.FindDefaultWalletInteract;
+import com.wallet.crypto.trustapp.interact.GetDefaultWalletBalance;
 import com.wallet.crypto.trustapp.router.ConfirmationRouter;
 
 import java.math.BigInteger;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 public class SendViewModel extends BaseViewModel {
     private final MutableLiveData<Wallet> defaultWallet = new MutableLiveData<>();
-    private final MutableLiveData<Transaction> transaction = new MutableLiveData<>();
     private final ConfirmationRouter confirmationRouter;
+    private final GetDefaultWalletBalance getDefaultWalletBalance;
+    private Disposable balanceDisposable;
+    private static final long GET_BALANCE_INTERVAL = 8;
+    private final MutableLiveData<NetworkInfo> defaultNetwork = new MutableLiveData<>();
+    private final MutableLiveData<Map<String, String>> defaultWalletBalance = new MutableLiveData<>();
+    private final FindDefaultNetworkInteract findDefaultNetworkInteract;
+    private final FindDefaultWalletInteract findDefaultWalletInteract;
 
-    SendViewModel(ConfirmationRouter confirmationRouter) {
+
+    SendViewModel(ConfirmationRouter confirmationRouter, GetDefaultWalletBalance getDefaultWalletBalance,
+                  FindDefaultNetworkInteract findDefaultNetworkInteract, FindDefaultWalletInteract findDefaultWalletInteract) {
         this.confirmationRouter = confirmationRouter;
+        this.findDefaultNetworkInteract = findDefaultNetworkInteract;
+        this.getDefaultWalletBalance = getDefaultWalletBalance;
+        this.findDefaultWalletInteract = findDefaultWalletInteract;
+    }
+
+    public LiveData<NetworkInfo> defaultNetwork() {
+        return defaultNetwork;
+    }
+
+    public LiveData<Wallet> defaultWallet() {
+        return defaultWallet;
+    }
+
+    public LiveData<Map<String, String>> defaultWalletBalance() {
+        return defaultWalletBalance;
     }
 
     public void openConfirmation(Context context, String to, BigInteger amount, String contractAddress, int decimals, String symbol, boolean sendingTokens) {
         confirmationRouter.open(context, to, amount, contractAddress, decimals, symbol, sendingTokens);
     }
+
+    public void prepare() {
+        progress.postValue(true);
+        disposable = findDefaultNetworkInteract
+                .find()
+                .subscribe(this::onDefaultNetwork, this::onError);
+    }
+
+    public void getBalance() {
+        balanceDisposable = Observable.interval(0, GET_BALANCE_INTERVAL, TimeUnit.SECONDS)
+                .doOnNext(l -> getDefaultWalletBalance
+                        .get(defaultWallet.getValue())
+                        .subscribe(defaultWalletBalance::postValue, t -> {}))
+                .subscribe();
+    }
+
+    private void onDefaultNetwork(NetworkInfo networkInfo) {
+        defaultNetwork.postValue(networkInfo);
+        disposable = findDefaultWalletInteract
+                .find()
+                .subscribe(this::onDefaultWallet, this::onError);
+    }
+
+    private void onDefaultWallet(Wallet wallet) {
+        defaultWallet.setValue(wallet);
+        getBalance();
+    }
+
 }
