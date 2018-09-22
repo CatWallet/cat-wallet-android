60c5fb7a4255702f16ec95d998e0df4295a9ab25
Yusheng Ding <davidthinkleding@Gmail.com>
2018-09-21 23:56:04 -0700

    feature: working on internal browser

--- app/src/main/java/com/wallet/crypto/trustapp/ui/BrowserActivity.java
+++ app/src/main/java/com/wallet/crypto/trustapp/ui/BrowserActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.app.NavUtils;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
@@ -139,15 +141,46 @@ public class BrowserActivity extends BaseNavigationActivity {
 //        return false;
 //    }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_settings, menu);
+//
+//        //NetworkInfo networkInfo = viewModel.defaultNetwork().getValue();
+//        //if (networkInfo != null && networkInfo.name.equals(ETHEREUM_NETWORK_NAME)) {
+//        //    getMenuInflater().inflate(R.menu.menu_deposit, menu);
+//        //}
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            // Respond to the action bar's Up/Home button
+//            case android.R.id.home:
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        getMenuInflater().inflate(R.menu.send_menu, menu);
 
-        //NetworkInfo networkInfo = viewModel.defaultNetwork().getValue();
-        //if (networkInfo != null && networkInfo.name.equals(ETHEREUM_NETWORK_NAME)) {
-        //    getMenuInflater().inflate(R.menu.menu_deposit, menu);
-        //}
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_next: {
+                Intent intent = new Intent(this, TransactionsActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                this.startActivity(intent);
+            }
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
