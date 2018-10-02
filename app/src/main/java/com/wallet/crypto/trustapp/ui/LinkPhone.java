1dd0e1e326744cbc56fb6f64922794aea6035ac3
Yusheng Ding <davidthinkleding@Gmail.com>
2018-10-01 17:40:57 -0700

    hotfix:app crash due to idk

---
+++ app/src/main/java/com/wallet/crypto/trustapp/ui/LinkPhone.java
@@ -0,0 +1,12 @@
+package com.wallet.crypto.trustapp.ui;
+
+import android.os.Bundle;
+
+import com.wallet.crypto.trustapp.R;
+
+public class LinkPhone extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
