b69e63b442ef1317ed10899dfd8d3b7c127fed81
Yusheng Ding <davidthinkleding@Gmail.com>
2018-10-11 19:15:04 -0700

    savePoint: add email login

--- app/src/main/java/com/wallet/crypto/trustapp/util/accountUtils.java
+++ app/src/main/java/com/wallet/crypto/trustapp/util/accountUtils.java
@@ -66,12 +66,14 @@ public class accountUtils extends BaseActivity {
 
 
     public static boolean isPhoneValid(String phone) {
-        //TODO: Replace this with your own logic
         return phone.matches("^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$");
     }
 
     public static boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return true;
+    }
+
+    public static boolean isEmailValid(String email){
+        return email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
     }
 }
