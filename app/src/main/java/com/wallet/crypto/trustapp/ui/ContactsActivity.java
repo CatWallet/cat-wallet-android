1e1900477ad72d3e6dbcb7fe77a91bf604eea69b
Yusheng Ding <davidthinkleding@Gmail.com>
2018-12-10 18:11:44 -0800

    SavePoint:adding recycle view

--- app/src/main/java/com/wallet/crypto/trustapp/ui/ContactsActivity.java
+++ app/src/main/java/com/wallet/crypto/trustapp/ui/ContactsActivity.java
@@ -3,6 +3,8 @@ package com.wallet.crypto.trustapp.ui;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,10 @@ import com.wallet.crypto.trustapp.router.SendRouter;
 public class ContactsActivity extends BaseActivity{
 
     public String address;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,20 @@ public class ContactsActivity extends BaseActivity{
                 new SendRouter().open(getApplicationContext(), address);
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.contact_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new ContactRecycleAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
